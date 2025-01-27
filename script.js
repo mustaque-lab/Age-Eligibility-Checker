97ef5dd1ec82679c9b5a475657bf4fc1d64d7d6e
mustaque-lab <77643203+mustaque-lab@users.noreply.github.com>
2025-01-27 11:10:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+document.getElementById("checkButton")
+.addEventListener("click", function() {
+    const ageValue = parseInt(document.getElementById("ageInput").value);
+
+   const resultDiv = document.getElementById("result");
+   resultDiv.innerHTML = "";
+   
+   if (ageValue <= 120) {
+    if (ageValue >= 18) {
+        resultDiv.innerHTML += "<i>You are eligible to vote.</i><br/>";
+    }else{
+        resultDiv.innerHTML += "<i>You are not eligible to vote.</i><br/>";
+    }
+
+    if (ageValue >= 16) {
+        resultDiv.innerHTML += "<i>You are eligible to drive.</i><br/>";
+    }else{
+        resultDiv.innerHTML += "<i>You are not eligible to drive.</i><br/>";
+    }
+   } else {
+        resultDiv.innerHTML = "<b>---!Invalid Age!---</b>";
+   }
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
